9422f358c3e4c1e9a18c6bf26d95c0f6f04fe235
xian2023 <hjx2011@vip.qq.com>
2023-07-30 23:47:11 +0800

    构建

---
+++ web-mobile/cocos-js/bullet.wasm-04ff41d4.js
@@ -0,0 +1,11 @@
+System.register([], function (exports, module) {
+	'use strict';
+	return {
+		execute: function () {
+
+			var bullet_wasm = exports('default', new URL('assets/bullet.wasm-9d17e9bf.wasm', module.meta.url).href);
+
+		}
+	};
+});
+//# sourceMappingURL=bullet.wasm-04ff41d4.js.map
---
+++ web-mobile/cocos-js/wait-for-ammo-instantiation.f017a.js
@@ -0,0 +1,14 @@
+System.register(['./instantiated-b4706e79.js'], function (exports) {
+	'use strict';
+	return {
+		setters: [function (module) {
+			exports('default', module.i0);
+		}],
+		execute: function () {
+
+
+
+		}
+	};
+});
+//# sourceMappingURL=wait-for-ammo-instantiation.js.map
