76f9ea881e70abd38067a7dccac0947828beb7cb
z503722728 <503722728@qq.com>
2025-05-27 12:37:57 +0800

    1

--- web-mobile/application.3e1ea.js
+++ web-mobile/application.3e1ea.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.d0df5.json';
+          this.settingsPath = 'src/settings.bb543.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- web-mobile/index.f9e93.js
+++ web-mobile/index.f9e93.js
@@ -1,4 +1,4 @@
-System.register(["./application.653ec.js"], function (_export, _context) {
+System.register(["./application.3e1ea.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
