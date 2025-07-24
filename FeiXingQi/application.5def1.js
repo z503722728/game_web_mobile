91e45926b71e7921d9a82f527f6ffd887d8b03bb
z503722728 <503722728@qq.com>
2025-07-24 12:58:49 +0800

    1

--- FeiXingQi/application.5def1.js
+++ FeiXingQi/application.5def1.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.2d0f4.json';
+          this.settingsPath = 'src/settings.8ced1.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
