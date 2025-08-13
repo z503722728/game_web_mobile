795766fcbed7f3c1b99a79bb6e87cc206143465e
z503722728 <503722728@qq.com>
2025-08-13 11:32:33 +0800

    1

--- FeiXingQi/application.a0d54.js
+++ FeiXingQi/application.a0d54.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.5161b.json';
+          this.settingsPath = 'src/settings.daee5.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- FeiXingQi/index.0d895.js
+++ FeiXingQi/index.0d895.js
@@ -1,4 +1,4 @@
-System.register(["./application.83b39.js"], function (_export, _context) {
+System.register(["./application.a0d54.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
