b9557b94b972590d0c1b59371933bb42f025e3ad
z503722728 <503722728@qq.com>
2025-08-22 12:36:57 +0800

    1

--- FeiXingQi/index.5a6f4.js
+++ FeiXingQi/index.5a6f4.js
@@ -1,4 +1,4 @@
-System.register(["./application.68686.js"], function (_export, _context) {
+System.register(["./application.e6e64.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
