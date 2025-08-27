f5bd68431e9c058d83cea3e7dfe984429be709ad
z503722728 <503722728@qq.com>
2025-08-27 11:34:39 +0800

    1

--- FeiXingQi/index.4be20.js
+++ FeiXingQi/index.4be20.js
@@ -1,4 +1,4 @@
-System.register(["./application.cec22.js"], function (_export, _context) {
+System.register(["./application.4494d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
