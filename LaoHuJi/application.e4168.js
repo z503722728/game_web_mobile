2416bc952285675074e1a17992361bd311fb1bb9
z503722728 <503722728@qq.com>
2025-08-27 11:43:12 +0800

    1

--- LaoHuJi/application.e4168.js
+++ LaoHuJi/application.e4168.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.67a93.json';
+          this.settingsPath = 'src/settings.a1794.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
