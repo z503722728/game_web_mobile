412c11b0aa79dbcd3effc5ae75dd495ea165a850
z503722728 <503722728@qq.com>
2025-09-04 12:16:17 +0800

    1

--- LaoHuJi/application.89ed8.js
+++ LaoHuJi/application.89ed8.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.1b3a2.json';
+          this.settingsPath = 'src/settings.cbdc7.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- LaoHuJi/index.ec7eb.js
+++ LaoHuJi/index.ec7eb.js
@@ -1,4 +1,4 @@
-System.register(["./application.646c3.js"], function (_export, _context) {
+System.register(["./application.89ed8.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
