4efab2eecd6a255548b3a70cbea63528281f3d90
z503722728 <503722728@qq.com>
2025-10-30 10:40:05 +0800

    1

--- NiePaoPao/application.ba967.js
+++ NiePaoPao/application.ba967.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.98bc5.json';
+          this.settingsPath = 'src/settings.b311b.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- NiePaoPao/index.6c7a4.js
+++ NiePaoPao/index.6c7a4.js
@@ -1,4 +1,4 @@
-System.register(["./application.b7a28.js"], function (_export, _context) {
+System.register(["./application.ba967.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
