354ee533bf7870287560d0bee61e094abe7c502a
z503722728 <503722728@qq.com>
2025-12-17 15:30:13 +0800

    弹珠机

--- DanZhuJi/application.abedb.js
+++ DanZhuJi/application.abedb.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.7bcc2.json';
+          this.settingsPath = 'src/settings.e6393.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- DanZhuJi/index.a0ef2.js
+++ DanZhuJi/index.a0ef2.js
@@ -1,4 +1,4 @@
-System.register(["./application.f3b02.js"], function (_export, _context) {
+System.register(["./application.abedb.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
